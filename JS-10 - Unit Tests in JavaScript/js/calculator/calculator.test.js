1ef1e7c94a1d93fc80a9c2719d7b18d9c2b61843
Rachel Isaac-Wilson <rachel.isaac.wilson@gmail.com>
2020-06-02 14:53:11 +0100

    Tasks

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
@@ -7,11 +7,36 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds -4 + 8 to equal 4', () => {
   expect(calculator.add(-4, 8)).toBe(4);
 });
-
+ 
 test('subtract 5 - 4 to equal 1', () => {
   expect(calculator.subtract(5, 4)).toBe(1);
 });
 
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
+});
+
+//## Part 2: Implement Calculator test
+
+//1. Implement 2 unit tests for the *divide* function.
+test('divide 366 by 6 to equal 61', () => {
+  expect(calculator.divide(366, 6)).toBe(61);
+});
+
+test('divide 10 / 2 to equal 5', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+//2. Implement 2 unit tests for the *multiply* function.
+test('multiply 8 * 36 to 288', () => {
+  expect(calculator.multiply(8, 36)).toBe(288);
+});
+
+test('multiply 12 * 4 to equal Infinity', () => {
+  expect(calculator.multiply(12, 4)).toBe(48);
+});
+
+// Challenge - Divide
+test('divide 5 / 0 to equal 5', () => {
+  expect(calculator.divide(5, 0)).toBe(5);
 });
--- JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
+++ JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
@@ -16,4 +16,30 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+//## Part 3: Implement UserController test
 
+//1. Implement 1 test for the *add* function that verifies a user that is not in the users' list.
+test('add user to userController', () => {
+  let user = new User(5678,"Milo", "milo@generation.org");
+  userController.add(user);    
+  expect(userController.getUsers()).toContain(user);
+});
+
+//2. Implement 1 test for the *remove* function that verifies a user that is not in the users' list.
+test('remove user to userController', () => {    
+  let user = new User(5678,"Milo", "milo@generation.org");    
+  userController.remove(user);
+  expect(userController.users).not.toContain(user);
+});
+
+//3. Implement 2 unit tests for the *findByEmail* function.
+test('find user by email ', () => {
+  userController.findByEmail('santiago@generation.org');
+  expect(userController.findByEmail('santiago@generation.org')).toEqual({"email": "santiago@generation.org", "id": 1234, "name": "Santiago"});
+});
+
+//4. Implement 2 unit tests for the *findById* function.
+test('find user by Id', () => {
+  userController.findById(1234);
+  expect(userController.findById(1234)).toEqual({"email": "santiago@generation.org", "id": 1234, "name": "Santiago"});
+  });
